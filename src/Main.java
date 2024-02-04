0da90c6b315c3de9f028f5bb701947e237b36329
m-sorokina <pdtt.lm@gmail.com>
2024-02-04 20:48:19 +0100

    try

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Select the number within range from 1 to 1000");
+
+        int numberFirst = 1;
+        int numberLast = 1024;
+        int numberMiddle = (numberFirst + numberLast) / 2;
+
+        while (numberFirst < numberLast) {
+            System.out.printf("Is your number greater %d (y/n) : ", numberMiddle);
+            String answer = in.nextLine();
+            if (answer.equals("y")) {
+                numberFirst = numberMiddle + 1;
+            } else {
+                numberLast = numberMiddle;
+            }
+            numberMiddle = (numberFirst + numberLast) / 2;
+        }
+        System.out.printf("Your number is %d %n", numberMiddle);
+    }
+}
